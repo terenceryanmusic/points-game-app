6072ca97e598bac27ee6cc11c259e4db18f21912
terenceryanmusic <53048309+terenceryanmusic@users.noreply.github.com>
2022-05-01 09:55:53 +0100

    added scoreboard

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
-
+import Scoreboard from './Scoreboard';
 const Player = () => {
 	const players = Object.keys(localStorage).map((keys) => localStorage.getItem(keys));
+
+	console.log(players);
+
 	return (
-		<section className='flex flex-col items-center'>
+		<section className='flex flex-col justify-around items-center'>
 			{players.map((player) => (
 				<article
 					className='grid justify-center items-center bg-slate-800 h-auto p-12 w-96 mb-12 rounded-xl'
 					key={player.key}
 					player={player}>
 					{player}
-					<div className='flex justify-center items-center mt-4'>
-						<button className='w-20 h-8 mr-4 rounded-md bg-yellow-600'>1 point</button>
-						<button className='w-20 h-8 mr-4 rounded-md bg-orange-600'>3 points</button>
-						<button className='w-20 h-8 rounded-md bg-green-600'>5 points</button>
-					</div>
+					<Scoreboard />
 				</article>
 			))}
 		</section>
